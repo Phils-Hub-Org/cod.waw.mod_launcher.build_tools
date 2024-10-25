ffb7f5e11288e076d28cdd06351db901ab180f50
Phil81334 <philgibsongames@outlook.com>
2024-10-25 01:27:41 +0100

    update

--- build_mod_ff.py
+++ build_mod_ff.py
@@ -55,63 +55,33 @@ WHEN modName.iwd is NOT present in AppData
 
 """ NOTE
 (1):
-    modName.iwd only gets copied to mods > appdata if its not present in the appdata/mods folder.
-    The actual building of the modName.iwd file is obv not done here.
+    When testing, you will need to replace the below 'wawRootDir' with your actual WAW root directory
+    as well as the 'modName' with the actual name of your mod.
 
 (2):
-    When using this in a GUI application, you will need to grab the text from your mod.csv widget section and paste it into the mod.csv file before copying it from mods > zone_source.
-    I've added the logic to copy content from mod.csv in mod folder to zone_source folder. So all you need to do for a GUI-based application is copy text from mod.csv widget-section to mod.csv file in mod folder then this module can take care of the rest.
+    The default stock mod launcher behaviour is to copy modName.iwd to appdata/mods folder if it is not present there.
+    So this module does take care of that, but for the actual building of the .iwd, check out the 'build_iwd.py' module.
 
 (3):
-    When testing, you will need to replace the below 'wawRootDir' with your actual WAW root directory
-    as well as the 'MOD_NAME' with the actual name of your mod.
-
-Below vars:
-    UPPERCASE: Used globally (scope: module) and locally (scope: function)
-    lowercase: used globally (scope: module)
+    When using this in a GUI application, you will need to grab the text from your mod.csv widget section and paste it into the mod.csv file before copying it from mods > zone_source.
+    I've added the logic to copy content from mod.csv in mod folder to zone_source folder. So all you need to do for a GUI-based application is copy text from mod.csv widget-section to mod.csv file in mod folder then this module can take care of the rest.    
 """
 
 import os, sys, subprocess, shutil
 
-wawRootDir = r'D:\SteamLibrary\steamapps\common\Call of Duty World at War'
-BIN_DIR = os.path.join(wawRootDir, 'bin')
-
-MODS_DIR = os.path.join(wawRootDir, 'mods')
-MOD_NAME = 'zm_tst1'
-MOD_DIR = os.path.join(MODS_DIR, MOD_NAME)
-
-ZONESOURCE_DIR = os.path.join(wawRootDir, 'zone_source')
-ZONE_ENGLISH_DIR = os.path.join(wawRootDir, 'zone', 'english')
-
-homeDir = os.path.expanduser('~')
-appdataDir = os.path.join(homeDir, 'AppData')
-activisionDir = os.path.join(appdataDir, 'Local', 'Activision')
-activisionModsDir = os.path.join(activisionDir, 'CoDWaW', 'mods')
-ACTIVISION_MOD_DIR = os.path.join(activisionModsDir, MOD_NAME)
-
-def copyModCsvFromModToZoneSource():
-    # print(f'\nBuild mod.csv start')
-    # print(f"############################## ---/--/--- ##############################\n")
-
-    shutil.copy2(os.path.join(MOD_DIR, 'mod.csv'), os.path.join(ZONESOURCE_DIR, 'mod.csv'))
+def copyModCsvFromModToZoneSource(modDir: str, zoneSourceDir: str) -> None:
+    shutil.copy2(os.path.join(modDir, 'mod.csv'), os.path.join(zoneSourceDir, 'mod.csv'))
     
-    print(f"Copying  {os.path.join(MOD_DIR, 'mod.csv')}")
-    print(f"     to  {os.path.join(ZONESOURCE_DIR, 'mod.csv')}")
+    print(f"Copying  {os.path.join(modDir, 'mod.csv')}")
+    print(f"     to  {os.path.join(zoneSourceDir, 'mod.csv')}")
 
-    # print(f'\n############################## ---/--/--- ##############################')
-    # print(f'Build mod.csv end\n')
-
-def buildModFf():
-    # print(f'\nBuild mod.ff start')
-    # print(f"############################## ---/--/--- ##############################\n")
-    
-    args = ['linker_pc', '-nopause', '-language', 'english', '-moddir', MOD_NAME, 'mod']
-    # print(f'Args: {' '.join(args)}\n')
+def buildModFf(modName: str, binDir: str) -> None:
+    args = ['linker_pc', '-nopause', '-language', 'english', '-moddir', modName, 'mod']
 
     # Use Popen to run the linker asynchronously
     process = subprocess.Popen(
         args,
-        cwd=BIN_DIR,
+        cwd=binDir,
         shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.PIPE,
@@ -124,61 +94,41 @@ def buildModFf():
         if output == '' and process.poll() is not None:
             break
         if output:
-            # print(f'INFO: {output.strip()}')
             print(output.strip())
 
     # Capture the stderr output after the process finishes
     stderr = process.stderr.read()
     if stderr:
-        # print(f'ERROR: {stderr.strip()}')
         print(stderr.strip())
 
-    # print(f'\n############################## ---/--/--- ##############################')
-    # print(f'Build mod.ff end\n')
-
-def moveModFfFromZoneEnglishToMod():
-    # print(f'\nMove mod.ff start')
-    # print(f"############################## ---/--/--- ##############################\n")
-    
-    modFfSource = os.path.join(ZONE_ENGLISH_DIR, 'mod.ff')
-    modFfDest = os.path.join(MOD_DIR, 'mod.ff')
+def moveModFfFromZoneEnglishToMod(zoneEnglishDir: str, modDir: str) -> None:
+    modFfSource = os.path.join(zoneEnglishDir, 'mod.ff')
+    modFfDest = os.path.join(modDir, 'mod.ff')
 
     shutil.move(modFfSource, modFfDest)
 
-    print(f"Moving  {os.path.join(ZONE_ENGLISH_DIR, 'mod.ff')}")
-    print(f"    to  {os.path.join(MOD_DIR, 'mod.ff')}")
+    print(f"Moving  {os.path.join(zoneEnglishDir, 'mod.ff')}")
+    print(f"    to  {os.path.join(modDir, 'mod.ff')}")
 
-    # print(f'\n############################## ---/--/--- ##############################')
-    # print(f'Move mod.ff end\n')
+def copyModFfFromModToActivisionMod(activisionModDir: str, modDir: str) -> None:
+    if not os.path.exists(activisionModDir):
+        os.makedirs(activisionModDir)
 
-def copyModFfFromModToActivisionMod():
-    # print(f'\nCopy mod.ff start')
-    # print(f"############################## ---/--/--- ##############################\n")
-    
-    if not os.path.exists(ACTIVISION_MOD_DIR):
-        os.makedirs(ACTIVISION_MOD_DIR)
-
-    modFfSource = os.path.join(MOD_DIR, 'mod.ff')
-    modFfDest = os.path.join(ACTIVISION_MOD_DIR, 'mod.ff')
+    modFfSource = os.path.join(modDir, 'mod.ff')
+    modFfDest = os.path.join(activisionModDir, 'mod.ff')
 
     shutil.copy2(modFfSource, modFfDest)
 
     print(f"Copying  {modFfSource}")
     print(f"     to  {modFfDest}")
 
-    # print(f'\n############################## ---/--/--- ##############################')
-    # print(f'Copy mod.ff end\n')
-
 # Just a nice touch that the stock launcher has where it ensures the modName.iwd is present in appdata/mods folder during the mod.ff stage.
-def copyIwdFromModToActivisionMod():
-    # print(f'\nCopy modName.iwd start')
-    # print(f"############################## ---/--/--- ##############################\n")
-    
-    if not os.path.exists(ACTIVISION_MOD_DIR):
-        os.makedirs(ACTIVISION_MOD_DIR)
+def copyIwdFromModToActivisionMod(activisionModDir: str, modDir: str, modName: str) -> None:
+    if not os.path.exists(activisionModDir):
+        os.makedirs(activisionModDir)
 
-    modIwdSource = os.path.join(MOD_DIR, f'{MOD_NAME}.iwd')
-    modIwdDest = os.path.join(ACTIVISION_MOD_DIR, f'{MOD_NAME}.iwd')
+    modIwdSource = os.path.join(modDir, f'{modName}.iwd')
+    modIwdDest = os.path.join(activisionModDir, f'{modName}.iwd')
     
     # step 1: check if present in root/mods
     if os.path.exists(modIwdSource):
@@ -193,23 +143,32 @@ def copyIwdFromModToActivisionMod():
     print(f"Copying  {modIwdSource}")
     print(f"     to  {modIwdDest}")
 
-    # print(f'\n############################## ---/--/--- ##############################')
-    # print(f'Copy modName.iwd end\n')
-
-def teardown(message):
+def teardown(message: str) -> None:
     print(message)
     sys.exit(1)
 
 if __name__ == '__main__':
+    # change these 2 as needed
+    modName = 'zm_tst1'
+    wawRootDir = r'D:\SteamLibrary\steamapps\common\Call of Duty World at War'
+
+    # you can leave these alone
+    binDir = os.path.join(wawRootDir, 'bin')
+    modsDir = os.path.join(wawRootDir, 'mods')
+    modDir = os.path.join(modsDir, modName)
+    zoneSourceDir = os.path.join(wawRootDir, 'zone_source')
+    zoneEnglishDir = os.path.join(wawRootDir, 'zone', 'english')
+    activisionModDir = os.path.join(os.path.expanduser('~'), 'AppData', 'Local', 'Activision', 'CoDWaW', 'mods', modName)  # '~' = home dir
+
     CLEAN = True  # print()  # adds a newline
 
     # function calls
     steps = [
-        copyModCsvFromModToZoneSource,
-        buildModFf,
-        moveModFfFromZoneEnglishToMod,
-        copyModFfFromModToActivisionMod,
-        copyIwdFromModToActivisionMod
+        lambda arg1=modDir, arg2=zoneSourceDir: copyModCsvFromModToZoneSource(arg1, arg2),
+        lambda arg1=modName, arg2=binDir: buildModFf(arg1, arg2),
+        lambda arg1=zoneEnglishDir, arg2=modDir: moveModFfFromZoneEnglishToMod(arg1, arg2),
+        lambda arg1=activisionModDir, arg2=modDir: copyModFfFromModToActivisionMod(arg1, arg2),
+        lambda arg1=activisionModDir, arg2=modDir, arg3=modName: copyIwdFromModToActivisionMod(arg1, arg2, arg3),
     ]
 
     print()  # to separate from vs output
